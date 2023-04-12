a0c42a62f3049eb04ee8b4ede364501b3979921e
Enock <enock.mokua@student.moringaschool.com>
2023-04-12 20:48:51 +0300

    create the deadline component

--- src/components/countdown/countdown.js
+++ src/components/countdown/countdown.js
@@ -1,5 +1,34 @@
+import { useEffect, useState } from "react";
 import "./countdown.css";
 
-export default function CountDown() {
-  return <div className="countdown">This is the countdown</div>;
+export default function Countdown() {
+  const [countdown, setCountdown] = useState("");
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const now = new Date().getTime();
+      const deadline = new Date("May 5, 2023 00:00:00").getTime();
+      const distance = deadline - now;
+
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      setCountdown(
+        `${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`
+      );
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="countdown" aria-live="assertive">
+      <span>Countdown to May 5, 2023:</span>
+      <span>{countdown}</span>
+    </div>
+  );
 }
--- src/pages/landing.js
+++ src/pages/landing.js
@@ -1,9 +1,13 @@
 import "./landing.css";
-
+import CountDown from "../components/countdown/countdown";
 export default function Landing() {
   return (
     <main>
-      <section id="landing-section1">This is the landing page</section>
+      <section id="landing-section1">
+        <h1>We are under maintenance</h1>
+        <p>Dont fret we will be back soon</p>
+        <CountDown />
+      </section>
     </main>
   );
 }
