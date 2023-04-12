ac950aa99b23864d17f3d466d438ad5ea0c4ef08
Enock <enock.mokua@student.moringaschool.com>
2023-04-12 20:11:57 +0300

    create the default structure for the routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import "./App.css";
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Landing from "./pages/landing";
+import Header from "./components/header/header";
+import Footer from "./components/footer/footer";
+
 function App() {
-
-
   return (
-    <div className="App">
-      We are under maintenance
-      <h3>We will be back on:</h3>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
